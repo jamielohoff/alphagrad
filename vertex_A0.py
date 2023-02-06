b8a390912a5f3da1f759abd8c0ad0290d527b7c9
Jamie Lohoff <j.grieser@fz-juelich.de>
2023-02-06 10:49:50 +0100

    Implemented full training on Vertices.

--- vertex_A0.py
+++ vertex_A0.py
@@ -17,12 +17,15 @@ import equinox as eqx
 
 from tqdm import tqdm
 
+import graphax
+from graphax.graph import GraphState
 from graphax.game import VertexGame
 from graphax.elimination import forward, reverse
-from graphax.examples.random import construct_random_graph
+from graphax.examples.arbitrary import construct_random_graph
 from graphax.examples.helmholtz import construct_Helmholtz
+from graphax.examples.lif import construct_LIF
 
-from alphazero import A0_loss, get_masked_logits, preprocess_data, MCTSReplayMemory
+from alphazero import A0_loss, get_masked_logits, MCTSReplayMemory
 
 
 parser = argparse.ArgumentParser()
@@ -37,45 +40,31 @@ parser.add_argument("--seed", type=int,
                     default=42, help="Random seed.")
 
 parser.add_argument("--episodes", type=int, 
-                    default=500, help="Number of runs on random data.")
+                    default=2500, help="Number of runs on random data.")
 
 parser.add_argument("--replay_size", type=int, 
-                    default=2048, help="Size of the replay buffer.")
+                    default=4096, help="Size of the replay buffer.")
 
 parser.add_argument("--num_actions", type=int, 
                     default=11, help="Number of actions.")
 
 parser.add_argument("--num_simulations", type=int, 
-                    default=15, help="Number of simulations.")
-
-parser.add_argument("--iterations", type=int, 
-                    default=11, help="Number of iterations for each MCTS.")
-
-parser.add_argument("--rollout_length", type=int, default=11, 
-                    help="Duration of the rollout phase of the MCTS algorithm.")
+                    default=25, help="Number of simulations.")
 
 parser.add_argument("--batchsize", type=int, 
                     default=256, help="Learning batchsize.")
 
-parser.add_argument("--rollout_batchsize", type=int,
-                    default=32, 
+parser.add_argument("--rollout_batchsize", type=int, default=16, 
                     help="Batchsize for environment interaction.")
 
-parser.add_argument("--value_mixing", type=float, default=.5, 
-                   help="Mixing between predicted value and MCTS result.")
-
 parser.add_argument("--regularization", type=float, 
-                    default=0., help="Contribution of L2_regularization.")
+                    default=0., help="Contribution of L2 regularization.")
 
 parser.add_argument("--lr", type=float, 
                     default=3e-3, help="Learning rate.")
 
-
 parser.add_argument("--num_inputs", type=int, 
                     default=4, help="Number input variables.")
-
-parser.add_argument("--num_intermediates", type=int, 
-                    default=11, help="Number of intermediate variables.")
 
 parser.add_argument("--num_outputs", type=int, 
                     default=4, help="Number of output variables.")
@@ -90,12 +79,12 @@ wandb.run.name = args.name
 wandb.config = vars(args)
 
 NUM_INPUTS = args.num_inputs
-NUM_INTERMEDIATES = args.num_intermediates
+NUM_INTERMEDIATES = args.num_actions
 NUM_OUTPUTS = args.num_outputs
+SHAPE = (NUM_INPUTS+NUM_INTERMEDIATES, NUM_INTERMEDIATES+NUM_OUTPUTS)
 
 key = jrand.PRNGKey(args.seed)
-GS = construct_Helmholtz()
-
+GS = construct_Helmholtz() # construct_LIF() # 
 
 forward_gs = copy.deepcopy(GS)
 _, ops = forward(forward_gs, GS.get_info())
@@ -113,17 +102,18 @@ env = VertexGame(GS)
 
 nn_key, key = jrand.split(key, 2)
 subkeys = jrand.split(nn_key, 4)
-# NN = eqx.nn.MLP(15*15, 12, 128, 2, key=nn_key)
-NN = eqx.nn.Sequential([eqx.nn.Conv2d(1, 8, 7, key=subkeys[0]),
+
+NN = eqx.nn.Sequential([eqx.nn.Conv2d(1, 16, 7, key=subkeys[0]),
                         eqx.nn.Lambda(jnn.relu),
                         
-                        eqx.nn.Conv2d(8, 16, 7, key=subkeys[1]),
+                        eqx.nn.Conv2d(16, 32, 7, key=subkeys[1]),
                         eqx.nn.Lambda(jnn.relu),
                         
                         eqx.nn.Lambda(jnp.ravel),
-                        eqx.nn.Linear(144, 32, key=subkeys[2]),
+                        eqx.nn.Linear(288, 64, key=subkeys[2]),
                         eqx.nn.Lambda(jnn.relu),
-                        eqx.nn.Linear(32, 12, key=subkeys[3])])
+                        eqx.nn.Linear(64, NUM_INTERMEDIATES+1, key=subkeys[3])])
+
 
 batched_step = jax.vmap(env.step)
 batched_reset = jax.vmap(env.reset)
@@ -131,41 +121,21 @@ batched_one_hot = jax.vmap(jnn.one_hot, in_axes=(0, None))
 batched_get_masked_logits = jax.vmap(get_masked_logits, in_axes=(0, 0, None))
 
 
-def rollout(policy_network, init_states, batchsize, key):
-	def loop_fn(carry, _):
-		state, key = carry
-		subkey, key = jrand.split(key, 2)
-  
-		obs = state.edges[:, jnp.newaxis, :, :]
-		output = policy_network(obs)
-  
-		policy_logits = output[:, 1:]
-		masked_logits = batched_get_masked_logits(policy_logits, state, NUM_INTERMEDIATES)
-  
-		actions = jrand.categorical(subkey, masked_logits)
-		next_state, reward, _ = batched_step(state, actions)
-
-		return (next_state, key), reward
-
-	init_carry = (init_states, key)
-	_, rewards = lax.scan(loop_fn, init_carry, None, length=args.rollout_length)
-	return rewards.sum(axis=0)
-
-
 def recurrent_fn(params, rng_key, actions, state):
+	del rng_key
 	batchsize, nn_params = params
 	next_state, reward, _ = batched_step(state, actions) # dynamics function
 	next_obs = next_state.edges[:, jnp.newaxis, :, :]
- 
+
 	# prediction function
 	network = jtu.tree_map(lambda x, y: y if eqx.is_inexact_array(x) else x, NN, nn_params)
 	batch_network = jax.vmap(network)
- 
+
 	output = batch_network(next_obs)
 	policy_logits = output[:, 1:]
 	masked_logits = batched_get_masked_logits(policy_logits, state, NUM_INTERMEDIATES)
-	value = output[:, 0] 
- 
+	value = output[:, 0]
+
 	# On a single-player environment, use discount from [0, 1].
 	discount = jnp.ones(batchsize)
 	recurrent_fn_output = mctx.RecurrentFnOutput(reward=reward,
@@ -208,7 +178,6 @@ def environment_interaction(network, batchsize, init_carry):
 		# tree search derived targets for policy and value function
 		search_policy = policy_output.action_weights
 
-
 		# always take action recommended by tree search
 		action = policy_output.action
 
@@ -219,7 +188,7 @@ def environment_interaction(network, batchsize, init_carry):
 		flattened_obs = obs.reshape(batchsize, -1)
 		return (next_state, rews, key), jnp.concatenate([flattened_obs, search_policy, rews[:, jnp.newaxis], done[:, jnp.newaxis]], axis=1)
 
-	_, output = lax.scan(loop_fn, init_carry, None, length=args.iterations)
+	_, output = lax.scan(loop_fn, init_carry, None, length=NUM_INTERMEDIATES)
 
 	return jnp.stack(output).transpose(1, 0, 2)
 
@@ -234,14 +203,30 @@ def preprocess_data(data, idx=0):
     val = final_rew - rew
     return data.at[:, idx].set(val)
 
+@jax.jit
+def generate_batch(keys):
+	batchsize = keys.shape[0]
+	info = jnp.tile(GS.info, batchsize).reshape(batchsize, -1)
+	state = jnp.zeros((batchsize, NUM_INTERMEDIATES))
+
+	def loop_fn(_, key):
+		fraction = jrand.uniform(key, (1,))[0]
+		gs = construct_random_graph(NUM_INPUTS, NUM_INTERMEDIATES, NUM_OUTPUTS, key, fraction=fraction)
+		return _, gs.edges
+
+	_, edges = lax.scan(loop_fn, None, keys, length=batchsize)
+
+	return GraphState(info=info, edges=edges, state=state)
+
 
 def initialize_replay_buffer(buf, model, size, key):
 	print("Initializing replay buffer...")
-	batchsize = size // args.iterations + 1
+	batchsize = size // NUM_INTERMEDIATES + 1
 	keys = jrand.split(key, batchsize)
-	init_carry = (batched_reset(keys), jnp.full(batchsize, 0.), key)
+	batch_gs = generate_batch(keys)
+	init_carry = (batch_gs, jnp.full(batchsize, 0.), key)
 
-	transitions = eqx.filter_jit(environment_interaction)(model, batchsize, init_carry) # 
+	transitions = eqx.filter_jit(environment_interaction)(model, batchsize, init_carry)
 	transitions = preprocess_data(transitions, -2)
 	transitions = transitions.reshape(-1, transitions.shape[-1])
 
@@ -259,8 +244,9 @@ def train_agent(samples, network, opt_state, key):
 	# compute loss gradient compared to tree search targets and update parameters
 	key, subkey = jrand.split(key, 2)
 	subkeys = jrand.split(subkey, samples.shape[0])
-	obs = samples[:, 0:15*15].reshape(-1, 1, 15, 15)
-	search_policy = samples[:, 15*15:15*15+args.num_actions]
+	end_obs = SHAPE[0]*SHAPE[1]
+	obs = samples[:, 0:end_obs].reshape(-1, 1, SHAPE[0], SHAPE[1])
+	search_policy = samples[:, end_obs:end_obs+args.num_actions]
 	search_value = samples[:, -2]
 
 	loss, grads = eqx.filter_value_and_grad(A0_loss)(network, 
@@ -299,15 +285,18 @@ def differentiate(network):
 replay_buffer = initialize_replay_buffer(replay_buffer, NN, args.replay_size, key)
 
 pbar = tqdm(range(args.episodes))
+rewards = []
 for e in pbar:
 	# Data wrangling
 	keys = jrand.split(key, args.rollout_batchsize)
-	init_carry = (batched_reset(keys), jnp.full(args.rollout_batchsize, 0.), key)
- 
+	batch_gs = generate_batch(keys)
+
+	init_carry = (batch_gs, jnp.full(args.rollout_batchsize, 0.), key)
+
 	data = eqx.filter_jit(environment_interaction)(NN, args.rollout_batchsize, init_carry)
 	data = preprocess_data(data, -2)
 	data = data.reshape(-1, data.shape[-1])
- 
+
 	for i in range(data.shape[0]):
 		replay_buffer.push(data[i])
 
@@ -316,6 +305,8 @@ for e in pbar:
 	train_key, key = jrand.split(key)
 	loss, NN, opt_state = eqx.filter_jit(train_agent)(samples, NN, opt_state, train_key)
 	rew = differentiate(NN)
-	wandb.log({"loss": loss, "# computations": -rew})
+	print(rew, loss)
+	wandb.log({"loss": loss.tolist(), "# computations": -rew.tolist()})
+	rewards.append(-rew.tolist())
 	pbar.set_description(f"episode: {e}, loss: {loss}, return: {rew}")
 
